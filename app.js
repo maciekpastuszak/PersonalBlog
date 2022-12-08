379fec908943a95f9b2c209cec2265d85878c11c
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2022-12-08 09:03:52 +0100

    add mongoose and refactor app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 var _ = require('lodash');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -11,12 +12,21 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 const app = express();
 
-let posts = [];
-
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
+
+const postSchema = {
+  title: String,
+  content: String
+};
+
+const Post = mongoose.model("Post", postSchema);
+
+let posts = [];
 
 app.get("/", (req,res) => {
   res.render("home", {
@@ -25,26 +35,21 @@ app.get("/", (req,res) => {
     });
 })
 
-app.get("/about", (req,res) => {
-  res.render("about", {startingContent: aboutContent});
-})
-
-app.get("/contact", (req,res) => {
-  res.render("contact", {startingContent: contactContent});
-})
-
-app.get("/compose", (req,res) => {
+app.get("/compose", function(req, res){
   res.render("compose");
-})
+});
 
-app.post('/compose', (req,res) => {
-  let post = {
+app.post("/compose", function(req, res){
+
+  const post = new Post ({
     title: req.body.postTitle,
     content: req.body.postBody
-  };
-  posts.push(post);
+  });
+
+  post.save();
 
   res.redirect("/");
+
 });
 
 app.get("/posts/:postName", (req,res) => {
@@ -60,9 +65,7 @@ app.get("/posts/:postName", (req,res) => {
     }
   })
   
-  
 })
-
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
